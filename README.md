ccac7acb367a371d8589759d9d5e856d05a241cf
Haley Schafer <hschafer@umich.edu>
2018-07-27 10:18:51 +0000

    scrollspy fluid delay when navbar link clicked

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Haley Schafer Portfolio
 Stream One Project: User Centric Frontend Development - Code Institute 
 
+# Credits 
+The scrollSpy delay JavaScript function found through this tutorial here: https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
+
---
+++ index.js
@@ -0,0 +1,14 @@
+// Delayed scrolling to different sections of the page 
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
